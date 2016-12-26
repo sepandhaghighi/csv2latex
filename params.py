55ac271e8b6374c2d66de1ec72e240d04f03cc95
sepandhaghighi <sepand.haghighi@yahoo.com>
2016-12-27 00:57:15 +0330

    params file added

---
+++ params.py
@@ -0,0 +1,10 @@
+
+header={"static_1":"\\begin{table}[]\n","static_2":'\centering\n',"caption":'\caption',"label":'\label',"align":'\\begin{tabular}'}
+
+footer='''
+\end{tabular}
+\end{table}
+'''
+
+
+char_list=["%","&","_","#","$"]
