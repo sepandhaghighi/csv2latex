6493616020c42b30725b706482825f013209b9a7
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-07-30 22:45:47 +0430

    fix : __main__ and setup file added

--- csv2latex/csv2LaTeX.py
+++ csv2latex/csv2LaTeX.py
@@ -1,5 +1,5 @@
 import os
-from params import *
+from .params import *
 import sys
 def line(number,char="-"):
     '''
@@ -157,22 +157,7 @@ def make_dir():
             print("Access Error")
             sys.exit()
 
-if __name__=="__main__":
-    print(signature())
-    list_of_files=os.listdir()
-    csv_files=[]
-    make_dir()
-    for i in list_of_files:
-        if i.find(".csv")!=-1:
-            csv_files.append(i)
-    if len(csv_files)==0:
-        print("There is no csv file in this folder (Please copy your csv files here)")
-        print("Current Directory : "+str(os.getcwd()))
-        sys.exit()
-    else:
-        print(str(len(csv_files))+" CSV file found in this folder! ;-) ")
-        for item in csv_files:
-            create_latex(item)
+
 
 
 
